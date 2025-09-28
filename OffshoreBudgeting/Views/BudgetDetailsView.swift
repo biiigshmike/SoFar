ac94d0b046cfc76ac5f010181515ebc4e0b026ad
biiigshmike <brownmlee@icloud.com>
2025-09-28 07:19:53 -0700

    feat(view): share glass capsule container
    * OffshoreBudgeting/Views/Components/GlassCapsuleContainer.swift: add reusable glass capsule container with OS 26 support\n* OffshoreBudgeting/Views/BudgetDetailsView.swift: adopt shared capsule container\n* OffshoreBudgeting/Views/HomeView.swift: replace home container with shared variant

--- OffshoreBudgeting/Views/BudgetDetailsView.swift
+++ OffshoreBudgeting/Views/BudgetDetailsView.swift
@@ -749,50 +749,6 @@ private struct FilterBar: View {
     }
 }
 
-// MARK: - Shared Glass Capsule Container
-private struct GlassCapsuleContainer<Content: View>: View {
-    @EnvironmentObject private var themeManager: ThemeManager
-    @Environment(\.responsiveLayoutContext) private var layoutContext
-    @Environment(\.platformCapabilities) private var capabilities
-
-    private let content: Content
-    private let horizontalPadding: CGFloat
-    private let verticalPadding: CGFloat
-    private let contentAlignment: Alignment
-
-    init(
-        horizontalPadding: CGFloat = DS.Spacing.l,
-        verticalPadding: CGFloat = DS.Spacing.m,
-        alignment: Alignment = .leading,
-        @ViewBuilder content: () -> Content
-    ) {
-        self.content = content()
-        self.horizontalPadding = horizontalPadding
-        self.verticalPadding = verticalPadding
-        self.contentAlignment = alignment
-    }
-
-    var body: some View {
-        let capsule = Capsule(style: .continuous)
-
-        let decorated = content
-            .frame(maxWidth: .infinity, alignment: contentAlignment)
-            .padding(.horizontal, horizontalPadding)
-            .padding(.vertical, verticalPadding)
-            .contentShape(capsule)
-
-        if #available(iOS 26.0, macOS 26.0, tvOS 18.0, macCatalyst 26.0, *), capabilities.supportsOS26Translucency {
-            GlassEffectContainer {
-                decorated
-                    .glassEffect(.regular.interactive(), in: capsule)
-            }
-        } else {
-            // Classic OS: render inline with no capsule background
-            decorated
-        }
-    }
-}
-
 private extension View {
     func segmentedFill() -> some View {
         frame(maxWidth: .infinity)
---
+++ OffshoreBudgeting/Views/Components/GlassCapsuleContainer.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+// MARK: - GlassCapsuleContainer
+struct GlassCapsuleContainer<Content: View>: View {
+    @EnvironmentObject private var themeManager: ThemeManager
+    @Environment(\.responsiveLayoutContext) private var layoutContext
+    @Environment(\.platformCapabilities) private var capabilities
+
+    private let content: Content
+    private let horizontalPadding: CGFloat
+    private let verticalPadding: CGFloat
+    private let contentAlignment: Alignment
+
+    init(
+        horizontalPadding: CGFloat = DS.Spacing.l,
+        verticalPadding: CGFloat = DS.Spacing.m,
+        alignment: Alignment = .leading,
+        @ViewBuilder content: () -> Content
+    ) {
+        self.content = content()
+        self.horizontalPadding = horizontalPadding
+        self.verticalPadding = verticalPadding
+        self.contentAlignment = alignment
+    }
+
+    var body: some View {
+        let _ = themeManager
+        let _ = layoutContext
+
+        let capsule = Capsule(style: .continuous)
+        let decorated = content
+            .frame(maxWidth: .infinity, alignment: contentAlignment)
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
+            .contentShape(capsule)
+
+        if #available(iOS 26.0, macOS 26.0, tvOS 18.0, macCatalyst 26.0, *), capabilities.supportsOS26Translucency {
+            GlassEffectContainer {
+                decorated
+                    .glassEffect(.regular.interactive(), in: capsule)
+            }
+        } else {
+            decorated
+        }
+    }
+}
